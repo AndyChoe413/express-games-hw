29369da3af4d9ec19b46972edbbfe65a40c920c2
Andy Choe <Andrew.s.choe@gmail.com>
2021-05-23 19:37:19 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const logger = require("morgan");
+
+const gameRouter = require("./routes/gameRouter");
+const indexRouter = require("./routes/indexRouter");
+const app = express();
+
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use("/", indexRouter);
+app.use("/api/game", gameRouter);
+
+
+app.listen(3000, function () {
+  console.log(`Server is running on PORT: ${3000}!`);
+});
+
+// module.exports = app;
